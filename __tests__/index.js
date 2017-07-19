95cd705364034c58524fecd9e7ef6de7da2d9a0a
Stephen Edgar <stephen@netweb.com.au>
2017-07-19 22:45:38 +1000

    chore: Add ESLint and use `eslint-plugin-wordpress/recommended` shared config.

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,11 @@
-"use strict"
+'use strict';
 
 const isPlainObj = require( 'is-plain-obj' );
 
-test('index.js', () => {
-	const conf = require('../index.js');
-	expect(isPlainObj(conf)).toBe(true);
-	expect(isPlainObj(conf.env)).toBe(true);
-	expect(isPlainObj(conf.globals)).toBe(true);
-	expect(isPlainObj(conf.rules)).toBe(true);
+test( 'index.js', () => {
+	const conf = require( '../index.js' );
+	expect( isPlainObj( conf ) ).toBe( true );
+	expect( isPlainObj( conf.env ) ).toBe( true );
+	expect( isPlainObj( conf.globals ) ).toBe( true );
+	expect( isPlainObj( conf.rules ) ).toBe( true );
 });
